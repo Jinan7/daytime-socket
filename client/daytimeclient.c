c13f88a5774a3047fa87eac8b77afce41f086521
Jinan <lakanj25@gmail.com>
2025-09-09 00:56:46 +0100

    Add .gitignore file

--- client/daytimeclient.c
+++ client/daytimeclient.c
@@ -12,13 +12,21 @@ int main(int argc, char **argv)
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(13);
 	int n = inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	
+	//debug
 	printf("%d\n", n);
 	
 	
 	//create socket
 	int sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	
+	//debug
 	printf("%d\n", sockfd);
+	
+	//connect socket to server 
 	int status = connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+	
+	//debug
 	printf("%d\n", status);
 	return 0;		
 }	
